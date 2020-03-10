c63ad5ce5781c4cfc93662278c6d450730351b7d
Norald Alejo <norald.alejo@flysfo.com>
2020-03-10 11:20:42 -0700

    Initial Commit

---
+++ My Sample (iOS)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My Sample (iOS)
+//
+//  Created by Norald Alejo on 3/10/20.
+//  Copyright © 2020 Norald Alejo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
